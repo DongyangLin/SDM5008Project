943e6974ac130f4d4471eaffc7362e19df30110f
ZikunZhuang <12532840@mail.sustech.edu.cn>
2026-01-05 15:38:36 +0800

    update zh-report

--- scripts/rsl_rl/train.py
+++ scripts/rsl_rl/train.py
@@ -131,10 +131,10 @@ def main():
     # runner = OnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device)
     
     # HIM
-    # runner = HIMOnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device)
+    runner = HIMOnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device)
 
     # PIM
-    runner = PIMOnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device)
+    # runner = PIMOnPolicyRunner(env, agent_cfg.to_dict(), log_dir=log_dir, device=agent_cfg.device)
 
     # 在创建新的log_dir之前保存恢复路径 / Save resume path before creating a new log_dir
     if agent_cfg.resume:
